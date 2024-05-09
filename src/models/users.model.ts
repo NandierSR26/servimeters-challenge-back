6e8f4be4ac89e64b75e80bd9f5842532b50abc0f
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-05-09 08:51:15 -0500

    feat: :card_file_box: Users model created

---
+++ src/models/users.model.ts
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from "mongoose";
+
+export const UsersSchema = new Schema({
+  full_name: {
+    type: String,
+    required: [true, 'Full name is required']
+  },
+  email: {
+    type: String,
+    required: [true, 'E-mail is required'],
+    unique: true
+  },
+  phone: {
+    type: String,
+    required: [true, 'Phone is required']
+  },
+  password: {
+    type: String,
+    required: [true, 'Passowrd is required']
+  },
+  balance: {
+    type: Number,
+    default: 0
+  }
+});
+
+UsersSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret, options) {
+    delete ret._id;
+    delete ret.password;
+  },
+});
+
+export const UserModel = mongoose.model('User', UsersSchema);
