3dce51cf8807f4e5c7bc77a477e6fb1a124449b0
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-05-09 12:12:29 -0500

    feat: :lock: Created middleware to protect routes

---
+++ src/middlewares/validateJWT.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from "express";
+import { handleError } from "../config/handleReponses";
+import { UserModel,  } from "../models/users.model";
+import { validateToken } from "../config/jwt.adapter";
+import { Document } from 'mongoose'
+
+declare global {
+  namespace Express {
+      interface Request {
+          user?: Document; // Aquí puedes ajustar el tipo según lo necesites
+      }
+  }
+}
+
+export const validateJWT = async (req: Request, res: Response, next: NextFunction) => {
+  const authorization = req.header('Authorization');
+
+  if (!authorization) return handleError({ code: 401, message: 'No token provided', res });
+  if (!authorization.startsWith('Bearer ')) return handleError({ code: 401, message: 'Invalid Bearer token', res });
+
+  const token = authorization.split(' ').at(1) || '';
+
+  try {
+
+    const payload = await validateToken(token);
+    if (!payload) return handleError({ code: 401, message: 'Invalid token', res });
+
+    const user = await UserModel.findById(payload.id);
+    if (!user) return handleError({ code: 401, message: 'Invalid token - user', res });
+
+    req.body.user = user;
+    req.user = user;
+    next();
+
+  } catch (error) {
+    return handleError({ code: 500, message: 'Something went wrong', res });
+  }
+
+}
--- src/routes/index.routes.ts
+++ src/routes/index.routes.ts
@@ -3,10 +3,13 @@ import { authRouter } from './auth.routes';
 import { clasificationsRouter } from './clasifications.routes';
 import { gendersRouter } from './genders.routes';
 import { moviesRouter } from './movies.routes';
+import { validateJWT } from '../middlewares/validateJWT';
 
 const router = Router();
 
 router.use('/api/v1/auth', authRouter);
+
+router.use('/', validateJWT);
 router.use('/api/v1/clasifications', clasificationsRouter);
 router.use('/api/v1/genders', gendersRouter);
 router.use('/api/v1/movies', moviesRouter);
