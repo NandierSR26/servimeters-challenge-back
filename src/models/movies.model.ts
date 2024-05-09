4b033e2e2dc898641e1e6e5a4d6b15f08a64e2b7
Nandier Ruiz <nandier.ruiz@gmail.com>
2024-05-09 08:58:12 -0500

    feat: :card_file_box: Movies model created

---
+++ src/models/movies.model.ts
@@ -0,0 +1,43 @@
+import mongoose, { Schema } from "mongoose";
+
+export const MoviesSchema = new Schema({
+  name: {
+    type: String,
+    required: [true, 'Movie name is required']
+  },
+  description: {
+    type: String,
+    required: [true, 'Movie description is required']
+  },
+  director: {
+    type: String
+  },
+  year: {
+    type: String
+  },
+  clasification: {
+    type: String,
+    required: [true, 'Clasification is required']
+  },
+  gender: {
+    type: String,
+    required: [true, 'Gender is required']
+  },
+  duration: {
+    type: String
+  },
+  price: {
+    type: Number,
+    required: [true, 'Movie price is required']
+  }
+})
+
+MoviesSchema.set('toJSON', {
+  virtuals: true,
+  versionKey: false,
+  transform: function (doc, ret, options) {
+    delete ret._id;
+  },
+})
+
+export const MoviesModel = mongoose.model('Movies', MoviesSchema);
